be15f1db33e83506be2072efcbb57a26dda5ee76
Ritheesh Kumar Dyagala <pg9892139@gmail.com>
2026-07-09 22:08:47 +0530

    Add files via upload

--- dsa_practice.py
+++ dsa_practice.py
@@ -1,21 +1,16 @@
-def reverse(arr,start,end):
-    while start<end:
-        arr[start],arr[end]=arr[end],arr[start]
-        start+=1
-        end-=1
-def rotate_matrix_rows(mat,k):
-     n=len(mat) if len(mat)>0 else -1
-     m=len(mat[0])
-     k%=m
-     for i in range(n):
-         reverse(mat[i],0,m-1)
-         reverse(mat[i],0,k-1)
-         reverse(mat[i],k,m-1)
-         
-     return mat
+def max_sum_submatrix(mat,k):
+    m=len(mat)
+    n=len(mat[0])
+    arr = [[0] * (n + 1) for _ in range(m + 1)]
+    max_sum=-float('inf')
 
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            arr[i][j]=-arr[i-1][j-1]+arr[i-1][j]+arr[i][j-1]+mat[i-1][j-1]
+    for i in range(k,m+1):
+        for j in range(k,n+1):
+            current_box_sum = arr[i][j] -arr[i-k][j] - arr[i][j-k] + arr[i-k][j-k]
+            max_sum=max(max_sum,current_box_sum)
+test=[[1,2,-1],[0,0,2]]
 
-test=[[1,2,3,4,5]]
-
-print(rotate_matrix_rows(test,7))
-
+print(max_sum_submatrix(test,2))
